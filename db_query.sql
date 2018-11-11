a15c9c808a5f59054a11a574e3034458f886413a
rahulsatheeshs <42373464+rahulsatheeshs@users.noreply.github.com>
2018-11-11 15:53:30 +0530

    changes in db
    new changes in db for new increment added

--- db_query.sql
+++ db_query.sql
@@ -1,27 +1,9 @@
 
 
-
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Sep 14, 2018 at 07:22 PM
--- Server version: 10.1.13-MariaDB
--- PHP Version: 5.6.21
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
 -- Database: `sms`
 --
+CREATE DATABASE IF NOT EXISTS `sms` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `sms`;
 
 -- --------------------------------------------------------
 
@@ -64,6 +46,13 @@ CREATE TABLE `login` (
   `password` varchar(15) NOT NULL,
   `user` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`usr_name`, `password`, `user`) VALUES
+('rahul@gmail.com', '123', 'admin');
 
 -- --------------------------------------------------------
 
@@ -121,7 +110,7 @@ CREATE TABLE `salary` (
 --
 
 CREATE TABLE `staff` (
-  `usr_name` varchar(20) NOT NULL,
+  `usr_name` int(11) NOT NULL,
   `name` varchar(15) NOT NULL,
   `designation` varchar(20) NOT NULL,
   `in_charge` varchar(10) NOT NULL,
@@ -132,8 +121,21 @@ CREATE TABLE `staff` (
   `qualification` varchar(20) NOT NULL,
   `gender` varchar(2) NOT NULL,
   `join_date` date NOT NULL,
-  `withdraw_date` date NOT NULL
+  `withdraw_date` date NOT NULL,
+  `blood_grp` varchar(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `staff`
+--
+
+INSERT INTO `staff` (`usr_name`, `name`, `designation`, `in_charge`, `dob`, `address`, `phno`, `email`, `qualification`, `gender`, `join_date`, `withdraw_date`, `blood_grp`) VALUES
+(1, 'rahul satheesh ', 'Teacher', '', '2018-11-11', 'kattakada,trivandrum', '0472-2803309', 'ra@g.com', 'A+', 'F', '2018-11-18', '0000-00-00', 'A+'),
+(2, 'rahul satheesh ', 'Teacher', '', '2018-11-11', 'kattakada,trivandrum', '0472-2803309', 'ra@g.com', 'A+', 'F', '2018-11-18', '0000-00-00', 'A+'),
+(3, 'gokul', 'Teacher', '', '2018-11-11', 'nedumangad', '0472-2803309', 'ra@g.com', 'A-', 'F', '2018-11-25', '0000-00-00', 'AB+'),
+(4, 'gokul', 'Teacher', '', '2018-11-11', 'nedumangad', '0472-2803309', 'ra@g.com', 'A-', 'F', '2018-11-25', '0000-00-00', 'AB+'),
+(5, '', 'Select', '', '0000-00-00', '', '', '', 'Select', 'F', '0000-00-00', '0000-00-00', 'Sel'),
+(6, 'rahul satheesh ', 'Select', '', '2018-11-18', '', '', '', 'Select', 'F', '0000-00-00', '0000-00-00', 'Sel');
 
 -- --------------------------------------------------------
 
@@ -142,9 +144,10 @@ CREATE TABLE `staff` (
 --
 
 CREATE TABLE `student` (
-  `usr_name` varchar(15) NOT NULL,
+  `usr_name` int(11) NOT NULL,
   `name` varchar(15) NOT NULL,
   `class` varchar(3) NOT NULL,
+  `division` varchar(1) NOT NULL,
   `gender` varchar(3) NOT NULL,
   `dob` date NOT NULL,
   `blood_grp` varchar(3) NOT NULL,
@@ -156,6 +159,17 @@ CREATE TABLE `student` (
   `parent_usr_name` varchar(15) NOT NULL,
   `passout_date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`usr_name`, `name`, `class`, `division`, `gender`, `dob`, `blood_grp`, `address`, `cast`, `religion`, `joined_class`, `add_date`, `parent_usr_name`, `passout_date`) VALUES
+(36, 'rahul satheesh ', '1', '', 'M', '2018-11-10', 'A+', 'kattakada,thiruvanan', 'Nair', 'Hindu', '1', '2018-11-10', '', '0000-00-00'),
+(37, '', 'Cla', '', 'F', '0000-00-00', 'Sel', '', 'Select', 'Select', 'Cla', '0000-00-00', '', '0000-00-00'),
+(38, '', 'Cla', '', 'F', '0000-00-00', 'Sel', '', 'Select', 'Select', 'Cla', '0000-00-00', '', '0000-00-00'),
+(39, '', 'Cla', '', 'F', '0000-00-00', 'Sel', '', 'Select', 'Select', 'Cla', '0000-00-00', '', '0000-00-00'),
+(40, '', 'Cla', '', 'F', '0000-00-00', 'Sel', '', 'Select', 'Select', 'Cla', '0000-00-00', '', '0000-00-00');
 
 -- --------------------------------------------------------
 
@@ -214,19 +228,35 @@ ALTER TABLE `salary`
 -- Indexes for table `staff`
 --
 ALTER TABLE `staff`
-  ADD KEY `usr_name` (`usr_name`);
+  ADD PRIMARY KEY (`usr_name`);
 
 --
 -- Indexes for table `student`
 --
 ALTER TABLE `student`
-  ADD KEY `usr_name` (`usr_name`);
+  ADD PRIMARY KEY (`usr_name`);
 
 --
 -- Indexes for table `teacher`
 --
 ALTER TABLE `teacher`
   ADD KEY `usr_name` (`usr_name`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `staff`
+--
+ALTER TABLE `staff`
+  MODIFY `usr_name` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `student`
+--
+ALTER TABLE `student`
+  MODIFY `usr_name` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
 -- Constraints for dumped tables
@@ -264,23 +294,8 @@ ALTER TABLE `salary`
   ADD CONSTRAINT `salary_ibfk_1` FOREIGN KEY (`usr_name`) REFERENCES `login` (`usr_name`);
 
 --
--- Constraints for table `staff`
---
-ALTER TABLE `staff`
-  ADD CONSTRAINT `staff_ibfk_1` FOREIGN KEY (`usr_name`) REFERENCES `login` (`usr_name`);
-
---
--- Constraints for table `student`
---
-ALTER TABLE `student`
-  ADD CONSTRAINT `student_ibfk_1` FOREIGN KEY (`usr_name`) REFERENCES `login` (`usr_name`);
-
---
 -- Constraints for table `teacher`
 --
 ALTER TABLE `teacher`
   ADD CONSTRAINT `teacher_ibfk_1` FOREIGN KEY (`usr_name`) REFERENCES `login` (`usr_name`);
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
